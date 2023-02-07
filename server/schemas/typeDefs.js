e479f443ca1999e86eb12484b4f74c82ab662014
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-02-06 21:13:41 -0600

    apollo server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const typeDefs = `
+    type Pokemon {
+        _id: ID
+        pokemonId: Int
+        name: String
+        height: Int
+        weight: Int
+        image: String
+        moves: [String]
+    }
+    type Trainer {
+        _id: ID
+        username: String
+        email: String
+        pokemon: [Pokemon]
+    }
+    type Query {
+        pokemons: [Pokemon]
+        pokemon(pokemonId: Int!): Pokemon
+        trainers: [Trainer]
+        trainer(_id: ID!): Trainer
+    }
+
+`;
+module.exports = typeDefs
--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const path = require('path')
 const connection = require('./config/connection')
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
+const typeDefs = require('./schemas/typeDefs')
+const resolvers = require('./schemas/resolvers')
 
 const PORT = process.env.PORT || 3001
 const app = express()
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
@@ -16,7 +23,12 @@ app.get('/', (req, res) => {
 })
 //.once is event listener from mongoose
 connection.once('open', async () => {
+    await apolloServer.start()
+    app.use('/graphql', expressMiddleware(apolloServer))
+
+
     app.listen(PORT, () => {
         console.log('Express server listening on http://localhost:${PORT}')
+        console.log(`Apollo GraphQL playground available at http://localhost:${PORT}/graphql`)
     })
 })
